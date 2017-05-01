199f73aa530af307e58ac78c1d872261e7ada122
DESKTOP-6P470OM\Ovidas <ovidijus.rakauskas@gmail.com>
2017-05-01 13:43:30 +0300

    Snake

---
+++ C#/snake/CFood.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public class CFood
+    {
+        protected const int _size = 4;
+        public Rectangle DrawRect = Rectangle.Empty;
+
+        private Point _location = Point.Empty;
+        public Point Location
+        {
+            get { return _location; }
+            set
+            {
+                _location = value;
+                // create our rectangle body
+                DrawRect = new Rectangle(_location.X - _size, _location.Y - _size, _size * 2, _size * 2);
+            }
+        }
+
+        public CFood(int __x, int __y)
+        {
+            Location = new Point(__x, __y);
+        }
+
+        public void Draw(Graphics __g)
+        {
+            Brush b = PickBrush();
+            __g.FillRectangle(b, DrawRect);
+        }
+
+            private Brush PickBrush()
+        {
+            Brush result = Brushes.Transparent;
+
+            Random rnd = new Random();
+
+            Type brushesType = typeof(Brushes);
+
+            PropertyInfo[] properties = brushesType.GetProperties();
+
+            int random = rnd.Next(properties.Length);
+            result = (Brush)properties[random].GetValue(null, null);
+
+            return result;
+        
+    }
+    }
+}
